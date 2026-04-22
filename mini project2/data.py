684788bc943dc29349f16355e553a60785189f10
Kuppa Rajesh <kupparajesh145@gmail.com>
2026-04-22 11:35:35 +0530

    Add files via upload

---
+++ mini project2/data.py
@@ -0,0 +1,10 @@
+Name = input("enter student name:")
+Age  = int(input("Enter student age:"))
+Marks = float(input("Enter student marks:"))
+passed = Marks >= 60
+
+print("-----student details-------")
+print(Name)
+print(Age)kaj
+print(Marks)
+print(passed)
